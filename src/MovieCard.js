2f6424c47a998b5b3e984ead988ea6c1280cdc24
Octowl <alsaff1987@gmail.com>
2019-02-03 10:00:13 +0300

    restyle moviecard

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,35 +1,20 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class MovieCard extends Component {
   render() {
     const movie = this.props.movie;
     return (
       <div className="col-lg-2 col-md-6 col-12">
-      <div
-        className="card"
-        style={{
-          margin: "20px",
-          backgroundColor: "white",
-          paddingTop: "7px"
-        }}
-      >
-        <img
-          className="card-img-top"
-          style={{ maxHeight: "200px", margin: "auto" }}
-          src={movie.image}
-          alt=""
-        />
-        <div className="card-body">
-          <h5 className="card-title">{movie.name}</h5>
-          <a
-            href={`/movies/${movie.id}`}
-            className="btn btn-outline-info"
-            style={{ backgroundColor: "white" }}
-          >
-            More Details
-          </a>
+        <div className="card m-3">
+          <img className="card-img-top" src={movie.image} alt={movie.title} />
+          <div className="card-body">
+            <h5 className="card-title">{movie.name}</h5>
+            <a href={`/movies/${movie.id}`} className="btn btn-outline-info">
+              More Details
+            </a>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
